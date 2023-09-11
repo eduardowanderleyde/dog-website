3c9f47d4b13875ed0d768c0f236ef6ecbed68934
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-09-11 15:25:27 -0300

    making components

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
 
 const LoginForm = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username, password}),
+    }).then(response => {
+      console.log(response);
+      return response.json();
+    }).then(json => {
+      console.log(json);
+    });
+  }
+
   return (
     <section>
       <h1>Login</h1>
-      <form action="">
-        <input type="text" 
-        value = {username}
-        onChange={({target}) => setUsername(target.value)}/>
-
-        <input type="text" 
-        value = {password}
-        onChange={({target}) => setPassword(target.value)}/>
+      <form action="" onSubmit={handleSubmit}>
+        <Input/>
         <button>Entrar</button>
         </form>
       <Link to = "/login/criar">Cadastro</Link>
