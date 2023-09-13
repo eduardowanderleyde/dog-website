d5b7d08b67bce2b1cb9887d337b0768869b7e6ce
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-09-13 15:53:35 -0300

    FIX - adjusting the styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return <div>
-    <h1 className={styles.footer}>Footer</h1>  </div>
+    <div className={styles.footer}>Footer</div>  </div>
 };
 
 export default Footer;
