2963b47dfec71f1f3ead4ca10ca4fff5d58e8139
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-09-13 19:01:07 -0300

    BUILD - Login create

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginCreate = () => {
   return (
-    <div>Login Criar</div>
+    <section className="animeLeft"><h1 className='title'>Cadastre-se</h1></section>
   )
 }
 
