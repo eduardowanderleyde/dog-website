208a676ab6702f4cf28f2d8ece667b509a52a4b3
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-08 22:49:05 -0300

    BUILT -Constructing FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -6,16 +6,20 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = ({photo}) => {
+const FeedModal = ({photo, setModalPhoto}) => {
   const {data, error, loading, request} = useFetch();
 
   React.useEffect(() => { 
     const {url, options} = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
+
+  function handleOutsideClick(event){
+    if(event.target === event.currentTarget) setModalPhoto(null);
+  }
   
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error= {error}/>}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -12,7 +12,7 @@ const PhotoContent = ({data}) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
               <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
               <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
