318a3c905a9744b4798731def7431f8c79b772de
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-16 13:09:35 -0300

    BUILT - Make the pages the doesnt exist an Error 404

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -26,6 +29,12 @@ const App = () => {
               </ProtectedRoute>
             }
         />
+        <Route path="foto/:id" element ={<Photo />} />
+        <Route path="perfil/:user" element ={<UserProfile />} />
+        <Route path="*" element ={<NotFound />} />
+
+
+
         </Routes>
         <Footer/>
         </UserStorage>
