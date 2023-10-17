e292fc5e35edfdeb75a407feae2686bf6e4c5149
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-16 21:17:04 -0300

    FIX - Changinh the impors os useMedia

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,7 +6,7 @@ import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg'
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg'
 import {ReactComponent as Sair} from '../../Assets/sair.svg'
 import styles from './UserHeaderNav.module.css'
-import useMedia from '../../Hooks/UseMedia'
+import useMedia from '../../Hooks/useMedia'
 
 
 const UserHeaderNav = () => {
